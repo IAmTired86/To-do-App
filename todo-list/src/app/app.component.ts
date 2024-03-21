09aa0cda71422982e71e0b0aa21f1f9b76ca00f3
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-03-21 13:45:22 +0700

    Day 2 : lesson 13

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -3,39 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import {CommonModule} from "@angular/common";
 import {TodoItemComponent} from "./todo-item/todo-item.component";
-import {TodoItem} from "./interfaces/todo-item";
+import {ListManagerComponent} from "./list-manager/list-manager.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InputButtonUnitComponent, CommonModule, TodoItemComponent],
+  imports: [RouterOutlet, InputButtonUnitComponent, CommonModule, TodoItemComponent, ListManagerComponent],
   template: `
     <h1>
       Welcome to {{ title }}!
     </h1>
 
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
-
-    <ul>
-      <li *ngFor="let todo of todoList">
-        <app-todo-item [item]="todo"></app-todo-item>
-      </li>
-    </ul>
+    <app-list-manager></app-list-manager>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'todo-list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
 }
---
+++ todo-list/src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoItem } from '../interfaces/todo-item';
+import {TodoItemComponent} from "../todo-item/todo-item.component";
+import {InputButtonUnitComponent} from "../input-button-unit/input-button-unit.component";
+
+@Component({
+  selector: 'app-list-manager',
+  standalone: true,
+  imports: [CommonModule, InputButtonUnitComponent, TodoItemComponent],
+  template: `
+    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todo of todoList">
+        <app-todo-item [item]="todo"></app-todo-item>
+      </li>
+    </ul>
+  `,
+  styleUrl: './list-manager.component.scss'
+})
+export class ListManagerComponent {
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
+}

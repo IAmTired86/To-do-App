f84fbddf33a0554a55973ae2d5f305c5b80fb18a
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-03-21 09:34:01 +0700

    Day 2 : lesson 6

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InputButtonUnitComponent],
+  imports: [RouterOutlet, InputButtonUnitComponent, CommonModule],
   template: `
     <h1>
       Welcome to {{ title }}!

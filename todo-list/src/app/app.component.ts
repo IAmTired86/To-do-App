56b10a887ca0d45595ec7fcc9b6672efe0ee70fb
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-03-21 10:57:37 +0700

    Day 2 : lesson 9

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -13,9 +13,21 @@ import {CommonModule} from "@angular/common";
     </h1>
 
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todo of todoList">{{ todo.title }}</li>
+    </ul>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
